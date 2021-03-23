f156e7b35b417412ebbe93e791e9b9920218afc2
LAPTOP-5IDLL1HJ\User <markwillowtree@gmail.com>
2021-03-23 19:41:28 +0000

    Added map zoom buttons and cleaned up Cyto initialisation and layout management

--- FightingFantasy.Mvc/wwwroot/js/CytoRepository.js
+++ FightingFantasy.Mvc/wwwroot/js/CytoRepository.js
@@ -1,20 +1,43 @@
 ﻿class CytoRepository {
     cy
 
-    constructor(cy) {
-        this.cy = cy;
+    constructor(playthrough, containerId) {
+        this.cy = cytoscape({
+            container: $(`#${containerId}`)
+            , style: cytoscape.stylesheet()
+            .selector('node')
+            .css({
+                'height': 'label',
+                'width': 'label',
+                'background-color': '#ffffff',
+                'text-color': '#000000',
+                'text-wrap': 'wrap',
+                'text-halign': 'center',
+                'text-valign': 'center',
+                'shape': 'rectangle',
+                'padding': '2px'
+            })
+        });
+
+        this.updateGraph(playthrough);
     }
 
     updateGraph(playthrough) {
-        cy.$('node').remove();
-        cy.$('edge').remove();
+        this.cy.$('node').remove();
+        this.cy.$('edge').remove();
 
         var startParagraph = playthrough.startParagraph;
 
-        cy.add([
-            { group: 'nodes', data: { id: `${startParagraph.id}` } }
+        this.cy.add([
+            {
+                group: 'nodes',
+                data: { id: `${startParagraph.id}` },
+                locked: false,
+                grabbable: false,
+                pannable: true
+            }
         ]);
-        cy.$(`#${startParagraph.id}`)
+        this.cy.$(`#${startParagraph.id}`)
             .css({ label: `${startParagraph.number} ${startParagraph.description}`  });
 
         console.log(`Adding node ${startParagraph.id}`);
@@ -23,14 +46,20 @@
         var curr = startParagraph.toParagraph;
 
         while (curr != undefined) {
-            cy.add([
-                { group: 'nodes', data: { id: `${curr.id}` } },
+            this.cy.add([
+                {
+                    group: 'nodes',
+                    data: { id: `${curr.id}` },
+                    locked: false,
+                    grabbable: false,
+                    pannable: true
+                },
                 { group: 'edges', data: { id: `${prev.id}${curr.id}`, source: `${prev.id}`, target: `${curr.id}`} }
             ]);
             
             console.log(`Adding node ${curr.id} and edge ${prev.id}${curr.id}`);
 
-            cy.$(`#${curr.id}`).css({ label: `${curr.number} ${curr.description}` });
+            this.cy.$(`#${curr.id}`).css({ label: `${curr.number} ${curr.description}` });
 
             console.log(`Adding description ${curr.description} on ${curr.id}`);
 
@@ -38,37 +67,81 @@
             curr = curr.toParagraph;
         }
 
-        var layout = cy.layout({
-            name: 'grid',
-            fit: true
-        });
+        var elements = this.cy.elements();
 
-        layout.run();
+        // if there is only one paragraph
+        if (playthrough.startParagraph.toParagraph == undefined) {
+            this.cy.fit(elements, 200);
+        }
+        else {
+            var layout = this.cy.layout({
+                fit: true,
+                name: 'grid'
+            });
+            layout.run();
+            this.cy.fit(elements, 20);
+        }
+    }
+
+    incrementZoom() {
+        var zoom = this.cy.zoom();
+        this.cy.zoom(zoom + 0.2);
+        //this.zoomLevel += 0.2;
+        //var layout = this.cy.layout({
+        //    fit: false,
+        //    name: 'grid',
+        //    zoom: this.zoomLevel
+        //});
+        //layout.run();
+    }
+
+    decrementZoom() {
+        var zoom = this.cy.zoom();
+        this.cy.zoom(zoom - 0.2);
+        //this.zoomLevel -= 0.2;
+        //var layout = this.cy.layout({
+        //    fit: false,
+        //    name: 'grid',
+        //    zoom: this.zoomLevel
+        //});
+        //layout.run();
+    }
+
+    resetZoom() {
+        var elements = this.cy.elements();
+
+        // if there is only one paragraph
+        if (playthrough.startParagraph.toParagraph == undefined) {
+            this.cy.fit(elements, 200);
+        }
+        else {
+            var layout = this.cy.layout({
+                fit: true,
+                name: 'grid'
+            });
+            layout.run();
+            this.cy.fit(elements, 20);
+        }
     }
 
 
     appendParagraph(currId, newId) {
         // add new node and edge to graph
-        cy.add([
+        this.cy.add([
             { group: 'nodes', data: { id: newId } },
             { group: 'edges', data: { id: `${currId}${newId}`, source: currId, target: newId } }
         ]);
 
-        cy.$(`#${newId}`).css({ label: 'Enter description' });
+        this.cy.$(`#${newId}`).css({ label: 'Enter description' });
 
-        var layout = cy.layout({
-            name: 'grid',
-            fit: true
-        });
-
-        layout.run();
+        this.resetZoom();
     }
 
     editParagraph(paragraphId, paragraphNumber, paragraphDescription) {
-        cy.$(`#${paragraphId}`).css({ label: `${paragraphNumber}. ${paragraphDescription}` });
+        this.cy.$(`#${paragraphId}`).css({ label: `${paragraphNumber}. ${paragraphDescription}` });
     }
 
     deleteParagraph(paragraphId) {
-        cy.remove(`#${paragraphId}`);
+        this.cy.remove(`#${paragraphId}`);
     }
 }

3a21acf25c304cb8b907212ddb8c1e39a41c6024
LAPTOP-5IDLL1HJ\User <markwillowtree@gmail.com>
2021-04-03 18:20:48 +0100

    Initalise character form inputs updated on dice roll

--- FightingFantasy.Angular/ClientApp/src/app/initialise-character/initialise-character.component.ts
+++ FightingFantasy.Angular/ClientApp/src/app/initialise-character/initialise-character.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, QueryList, ViewChildren } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PlaythroughStatModel } from '../services/apiClient';
 import { ApiService } from '../services/api.service';
 import { PlayThroughModel } from '../services/apiClient';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { Observable, from } from 'rxjs';
 import { concatAll, map, tap } from 'rxjs/operators';
 
@@ -12,42 +12,58 @@ import { concatAll, map, tap } from 'rxjs/operators';
   templateUrl: './initialise-character.component.html',
   styleUrls: ['./initialise-character.component.css']
 })
-export class InitialiseCharacterComponent implements OnInit {
+export class InitialiseCharacterComponent implements OnInit  {
   playthrough: PlayThroughModel;
   stats: PlaythroughStatModel[];
-  numDice: Number = 0;
+  numDice: number = 0;
 
+  @ViewChildren('formInput') formInputs: QueryList<ElementRef>;
   statsForm: FormGroup;
+  focusedInputElement: HTMLInputElement;
 
   constructor(private route: ActivatedRoute, private apiService: ApiService) {}
 
   ngOnInit(): void {
-    this.statsForm = new FormGroup({
-
-    });
-
+  
     this.route.params.pipe(
       map(params => from(this.apiService.client.getPlaythrough(params.playthroughId))),
       concatAll()
     ).subscribe(playthrough => {
+      // initialise model
       this.playthrough = playthrough;
       let stats = playthrough.startParagraph.stats;
+
+      // create form
+      this.statsForm = new FormGroup({});
         for (var i = 0; i < stats.length; i++) {
-          this.statsForm.addControl(stats[i].name, new FormControl(0, Validators.required));
+          if (stats[i].initNumDice > 0) {
+            this.statsForm.addControl(stats[i].name, new FormControl(0, [Validators.required, Validators.min(1)]));
+          }
+          else {
+            this.statsForm.addControl(stats[i].name, new FormControl(0, [Validators.required]));
+          }
         }
       });
   }
 
-  onFocus(src) {
-    this.numDice = src.dataset['dice'];
+  onFocus(src: EventTarget) {
+    var element = src as HTMLInputElement;
+    this.numDice = Number(element.dataset['dice']);
+    this.focusedInputElement = element;
+
   }
 
   onDiceRoll(value: number) {
     console.log('dice rolled ' + value); 
-  }
-  // onSubmit() {
-  //   var skills = Object.keys(this.statsForm.controls);
 
-  //   console.log(this.statsForm);
-  // }
+    if (this.focusedInputElement != undefined) {
+      setTimeout(() => {
+        this.statsForm.controls[this.focusedInputElement.name].setValue(value);
+      }, 500);
+    }
+  }
+
+  onSubmit() {
+    
+  }
 }

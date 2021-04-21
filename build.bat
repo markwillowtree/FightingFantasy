3877e85a18f3baede0ce23c5ef6e94817af19227
LAPTOP-5IDLL1HJ\User <markwillowtree@gmail.com>
2021-04-21 19:38:21 +0100

    Added build script

--- build.bat
+++ build.bat
@@ -1,7 +1,11 @@
 cd FightingFantasy.Api
 dotnet tool restore
 cd ..
+dotnet restore FightingFantasy.sln
 
 cd FightingFantasy.Angular/ClientApp
-npm install
-npm audit fix
+call npm install
+call npm audit fix
+cd ../../
+
+dotnet build FightingFantasy.sln
